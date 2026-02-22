9fda71486478ccb4fb88d74423f12976cedab51f
Abhay pratap <singhpratap1055@gmail.com>
2026-02-22 23:14:23 +0530

    Day-22

---
+++ q22.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to count nodes
+int countNodes(struct Node* head) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+// Function to print linked list
+void printList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    struct Node* head = NULL;
+    struct Node* tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        struct Node* newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    // Print list
+    printList(head);
+    printf("\n");
+
+    // Print count
+    int total = countNodes(head);
+    printf("Total Nodes: %d\n", total);
+
+    return 0;
+}
